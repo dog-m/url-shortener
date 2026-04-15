f29b8a67693de407fa16a48c3fc610887f9921cc
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2026-04-16 02:06:23 +0300

    Minor project preparations

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='static')
 
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Hello World"}
+    return {'message': 'Hello World'}
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse('./static/favicon.ico')
