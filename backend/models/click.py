5d86030dfdaadfa832caf632f303a3a359c0b569
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2026-06-02 13:57:21 +0300

    Linting + Minor formatting fix

--- backend/models/click.py
+++ backend/models/click.py
@@ -15,5 +15,5 @@ class ClickEventEntry(BaseDbModel):
     id: Mapped[uuid.UUID]       = mapped_column(UUID(), primary_key=True, index=True)
     url_id: Mapped[str]         = mapped_column(ForeignKey('urls.id', ondelete="CASCADE"), nullable=False)
     timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=now_UTC, index=True, nullable=False)
-    meta: Mapped[int]       = mapped_column(JSON(none_as_null=True), nullable=False)
+    meta: Mapped[int]           = mapped_column(JSON(none_as_null=True), nullable=False)
 
