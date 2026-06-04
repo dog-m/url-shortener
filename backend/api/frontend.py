8f665e8bea84703f8a8bcd4d1758904ff3f84e6b
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2026-06-04 17:35:10 +0300

    Attempt at adding misc site info

--- backend/api/frontend.py
+++ backend/api/frontend.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, RedirectResponse
 
 #
 
 frontend_router = APIRouter(tags=['frontend'])
+
 
 
 @frontend_router.get('/', response_class=FileResponse, include_in_schema=False)
@@ -14,10 +15,35 @@ async def root():
     )
 
 
+
+@frontend_router.get('/index.html', response_class=RedirectResponse, include_in_schema=False)
+async def root_alt():
+    return RedirectResponse('/')
+
+
+
 @frontend_router.get('/favicon.ico', response_class=FileResponse, include_in_schema=False)
 async def favicon():
     return FileResponse(
         './frontend/favicon.png',
         media_type='image/png',
+    )
+
+
+
+@frontend_router.get('/robots.txt', response_class=FileResponse, include_in_schema=False)
+async def robots():
+    return FileResponse(
+        './frontend/robots.txt',
+        media_type='text/plain',
+    )
+
+
+
+@frontend_router.get('/sitemap.xml', response_class=FileResponse, include_in_schema=False)
+async def sitemap():
+    return FileResponse(
+        './frontend/sitemap.xml',
+        media_type='text/xml',
     )
 
