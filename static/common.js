f4fd0e87c1399a8a6b717e1e246e76eccbc66281
Mikhail Onischuck <43421682+dog-m@users.noreply.github.com>
2026-09-03 14:19:17 +0300

    Simplify some frontend JS

--- static/common.js
+++ static/common.js
@@ -1,1 +1,17 @@
-// nothing here yet
+function to_local_datetime(utc) {
+    return new Date(utc + 'Z');
+}
+
+function to_local_datetime_ISO(utc) {
+    // https://stackoverflow.com/a/51643788
+    let t = to_local_datetime(utc);
+    let z = t.getTimezoneOffset() * 60 * 1000;
+    let tLocal = new Date(t - z);
+    return tLocal.toISOString().split('.')[0];
+}
+
+function localize_time(utc) {
+    let value = to_local_datetime(utc).toLocaleString();
+    document.write(`<time>${value}</time>`);
+}
+
